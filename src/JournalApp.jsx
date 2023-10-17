5da140116f625e641595d39f1d0866488d9e293e
Bryanzdruls <eltiobryanz@gmail.com>
2023-10-17 13:15:30 -0500

    Hash router corregido x2

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,5 +1,4 @@
 
-import { BrowserRouter,HashRouter } from "react-router-dom"
 import { AppRouter } from "./router/AppRouter"
 import { AppTheme } from "./theme"
 
@@ -7,9 +6,7 @@ import { AppTheme } from "./theme"
 export const JournalApp = () => {
   return (
     <AppTheme>
-      <HashRouter>
         <AppRouter/>
-      </HashRouter>
     </AppTheme>
   )
 }
